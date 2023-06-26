3db17b348034a389e7da309b4f4fad38dc0b79b1
brysonkatuu22 <brysonkatuu77@gmail.com>
2023-06-26 13:40:15 -0700

    update files

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
 int _strlen(char *s)
 {
 	int longi = 0;
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
 void print_rev(char *s)
 {
 	int longi = 0;
